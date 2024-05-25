de24335c0db50199aa7ce758e2851fcb125983a2
Mustafa Palta <Mustafapalta00@hotmail.com>
2024-05-25 22:15:19 +0300

    Character Animator - Texture vs.

---
+++ Guardian of the Village/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,33 @@
+using RPG.Movement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Controller
+{
+    public class PlayerController : MonoBehaviour
+    {
+        void Update()
+        {
+
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+        private void MoveToCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+
+            if (hasHit == true)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+        }
+    }
+}
+
---
+++ Guardian of the Village/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.AI;
+using UnityEngine.AI;
+
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+
+
+        void Update()
+        {
+            UpdateAnimator();
+
+        }
+
+        public void MoveTo(Vector3 hit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+    }
+
+}
